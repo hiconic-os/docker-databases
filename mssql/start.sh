256946a881cbf9a924464de1ecd05edcc124d26c
Peter Gazdik <peter.gazdik+bt@gmail.com>
2020-11-05 17:28:08 +0100

    Again fixing script for gitbash/mingw

--- mssql/start.sh
+++ mssql/start.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker run --name bt-test-mssql --rm -d -p 51433:1433 -e ACCEPT_EULA=Y -e SA_PASSWORD=cortex-10 --entrypoint /opt/mssql-tools/script/entrypoint.sh --mount type=bind,source="${SCRIPT_ROOT_DIR}/entrypoint,target=/opt/mssql-tools/script" mcr.microsoft.com/mssql/server:2017-latest
+docker run --name bt-test-mssql --rm -d -p 51433:1433 -e ACCEPT_EULA=Y -e SA_PASSWORD=cortex-10 --entrypoint //opt/mssql-tools/script/entrypoint.sh --mount type=bind,source="${SCRIPT_ROOT_DIR}/entrypoint,target=/opt/mssql-tools/script" mcr.microsoft.com/mssql/server:2017-latest
