781fed4f434046010f305cd8bd92ea78ec87accb
Peter Gazdik <peter.gazdik+bt@gmail.com>
2021-10-28 13:34:01 +0200

    mssql startup triggered by server logs, not sleep

--- mssql/entrypoint/import-data.sh
+++ mssql/entrypoint/import-data.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-# DB needs some seconds to start
-sleep 10
+# We are checking every second for a log indicating the server has started, so we can call out init.sql via sqlcmd
+__waitTillServerStarts() {
+	local i;
+	for (( i=0; i<30 ; i++ )) ; do
+		# Not sure what the exact signal is that the server is ready, but a message saying 'Starting up database 'tempdb'.' seems to be working fine.
+		# If it ever fails, use "docker logs bt-test-mssql" to see what other outputs from the server come later (the output from docker logs is the content of the errorlog file below).
+		# We have no idea why the regular logs end up in "errolog" file, but that's how it is
+		local logs=$(cat /var/opt/mssql/log/errorlog | grep "Starting up database 'tempdb'.")
+		if [ ! -z "$logs" ]; then return; fi
+		sleep 1;
+	done
+}
+
+
+__waitTillServerStarts;
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P cortex-10 -i /opt/mssql-tools/script/init.sql
---
+++ mssql/init-loop.sh
@@ -0,0 +1,16 @@
+# Use for troubleshooting.
+# This is similar to import-data.sh, but can be run externally, i.e. from the outside of the docker container.
+
+__waitTillServerStarts() {
+	local i;
+	for (( i=0; i<30 ; i++ )) ; do
+		local logs=$(docker logs bt-test-mssql 2>/dev/null | grep "Starting up database 'tempdb'.")
+		if [ ! -z "$logs" ]; then return; fi
+		echo "NOT READY YET";
+		sleep 1;
+	done
+}
+
+
+__waitTillServerStarts;
+docker exec bt-test-mssql //opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P cortex-10 -i //opt/mssql-tools/script/init.sql
